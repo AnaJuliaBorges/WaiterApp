2500b9158dd40a73694eb7629f4c06785178e33a
Ana Julia Borges <najuborgess@gmail.com>
2025-06-09 15:02:58 -0300

    feat: add try catch to list categories

--- src/app/useCases/categories/listCategories.ts
+++ src/app/useCases/categories/listCategories.ts
@@ -2,7 +2,14 @@ import { Request, Response } from "express";
 import { Category } from "../../models/Category";
 
 export async function listCategories(req: Request, res: Response){
-  const categories = await Category.find();
-  res.json(categories);
-  return
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+    return
+
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+
 }
