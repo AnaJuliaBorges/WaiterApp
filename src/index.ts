8e3de3cc1c4a06ef87eb9552f09800a894a9e8eb
Ana Julia Borges <najuborgess@gmail.com>
2025-06-09 12:24:18 -0300

    feat: setup mongo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-const PORT = 3001;
+mongoose.connect("mongodb://localhost:27017")
+  .then(() => {
+    const app = express();
+    const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Servidor está rodando em http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor está rodando em http://localhost:${PORT}`);
+    });
+
+  })
+  .catch(() => console.error("Erro ao conectar ao MongoDB"));
+
+
