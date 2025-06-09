cc8acd2f49f2985fa1fbb168335151997496aa2a
Ana Julia Borges <najuborgess@gmail.com>
2025-06-09 14:53:17 -0300

    feat: create routes and categories use cases

---
+++ src/app/useCases/categories/createCategory.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { Category } from "../../models/Category";
+
+export async function createCategories(req: Request, res: Response){
+  try {
+    const { icon, name } = req.body;
+
+    const category = await Category.create({
+      icon,
+      name
+    });
+
+    res.status(201).json(category);
+    return;
+
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './router';
+
 mongoose.connect("mongodb://localhost:27017")
   .then(() => {
     const app = express();
     const PORT = 3001;
 
+    app.use(express.json());
+    app.use(router)
+
     app.listen(PORT, () => {
       console.log(`Servidor está rodando em http://localhost:${PORT}`);
     });
-
   })
   .catch(() => console.error("Erro ao conectar ao MongoDB"));
 
